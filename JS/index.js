4baaa2a9580302a13be66311eeecc20a8b227835
dennis-tech <hedenniskibe@gmail.com>
2023-06-13 00:57:36 +0300

    refactored code in all files

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,21 @@
+document.getElementById('convertBtn').addEventListener('click', unitConverter);
 
-let measurement = document.getElementById("size-3");
-let meterz = document.getElementById("meters-1");
-let volumez = document.getElementById("volume-1");
-let massz = document.getElementById("mass-1");
+function unitConverter() {
+        let inputValue = document.getElementById("inputField").value;
+        
+        if(inputValue){
+                document.getElementById('lengthMeters').innerHTML= inputValue +" meters = " + 
+                Number(inputValue*3.2808).toFixed(3) + " feet "+ " | " + inputValue + " feet = "+ 
+                Number(inputValue/3.2808).toFixed(3) + " meters" ;
 
-function lengthConverter (valNum) {
-        meterz.innerHTML= valNum +" meters = " + Number(valNum*3.2808).toFixed(3) + " feet "+ " | " + valNum + " feet = "+ Number(valNum/3.2808).toFixed(3) + " meters" ;
-        volumez.innerHTML= valNum +" litres = " + Number(valNum*0.264172).toFixed(3) + " gallons "+ " | " + valNum + " gallons = "+ Number(valNum/0.264172).toFixed(3) + " litres" ;
-        massz.innerHTML= valNum +" kilos = " + Number(valNum*2.2).toFixed(3) + " pounds "+ " | " + valNum + " pounds= "+ Number(valNum/2.2).toFixed(3) + " kilos" ;
+                document.getElementById('volumeLitres').innerHTML= inputValue +" litres = " + 
+                Number(inputValue*0.264172).toFixed(3) + " gallons "+ " | " + inputValue + " gallons = "+ 
+                Number(inputValue/0.264172).toFixed(3) + " litres" ;
+
+                document.getElementById('massKilos').innerHTML= inputValue +" kilos = " + 
+                Number(inputValue*2.2).toFixed(3) + " pounds "+ " | " + inputValue + " pounds= "+ 
+                Number(inputValue/2.2).toFixed(3) + " kilos" ;
+        }
+        
 }
 
